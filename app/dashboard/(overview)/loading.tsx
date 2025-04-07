1487489b0944396d8e3e709b7db05bcd75fb692e
yanqinlan <2314741287@qq.com>
2025-04-07 17:53:06 +0800

    chapter 1-10

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: 严钦蓝
+ * @FilePath: \nextjs-dashboard\app\dashboard\loading.tsx
+ * @LastEditors: 严钦蓝
+ * @LastEditTime: 2025-04-07 16:40:11
+ * @Description: 
+ */
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-04-06 19:22:54
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-04-06 19:22:59
+ * @LastEditors: 严钦蓝
+ * @LastEditTime: 2025-04-07 17:51:08
  * @FilePath: \nextjs-dashboard\app\dashboard\layout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
